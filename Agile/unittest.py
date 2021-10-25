25e26fec18d8c9f333d483914824d112abd44688
Tarun <tarunsaraswat478@gmail.com>
2021-10-25 11:48:21 +0530

    Unit Testing

--- Agile/unittest.py
+++ Agile/unittest.py
@@ -1,12 +1,26 @@
-def test_sum():
-    assert sum([1, 2, 3]) == 6, "Should be 6"
+class diffadd:
+    a=0
+    b=0
+    def add(self):
+        return (self.a+self.b)
+    
+    def diff(self):
+        return (self.a-self.b)
 
-def test_sum_tuple():
-    assert sum((1, 2, 2)) == 5, "Should be 6"
+var=diffadd()
+
+def test_sum():
+    assert var.add()==6, "Should be 6"
+
+def test_sub():
+    assert  var.diff()==2, "Should be 2"
     
     
 
 if __name__ == "__main__":
+    
+    var.a=int(input())
+    var.b=int(input())
     test_sum()
-    test_sum_tuple()
+    test_sub()
     print("Everything passed")
